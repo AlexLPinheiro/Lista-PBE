b19a2164a4b65f00a2364c1356f05c66be1be2e6
antegegemonm <alexlp2k6@gmail.com>
2025-03-18 15:58:29 -0300

    Versão final da atividade

--- Ex16.py
+++ Ex16.py
@@ -3,6 +3,9 @@ y1 = int(input("Digite o valor de y1: "))
 x2 = int(input("Digite o valor de x2: "))
 y2 = int(input("Digite o valor de y2: "))
 
-resultado = 4 **1/2
+resultado = (((x2-x1)**2)+((y2-y1)**2))**(1/2)
 ##(((x2-x1)**2)+((y2-y1)**2))**1/2
-print(f"A distãncia é igual a: {resultado}")
+#√((x2 – x1)² + (y2 – y1)²).
+
+
+print(f"A distãncia é igual a: {resultado:.3f}")
